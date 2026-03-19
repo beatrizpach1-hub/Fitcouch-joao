c1f45bca9e6ff92869160337ed3bd42333cfc06c
beatrizpach1-hub <beatrizpach1@gmail.com>
2026-03-19 11:52:48 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,10 @@
-const CACHE = 'jmpt-v1';
-const FILES = ['/'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-  self.skipWaiting();
-});
-
+// Clear all caches and unregister
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+    .then(() => self.registration.unregister())
   );
 });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
